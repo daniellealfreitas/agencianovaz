63557ca3a5eb2eaf56d2d826c29a418cbb8ddecf
Daniel Leal Freitas <dbleall@gmail.com>
2024-10-23 04:01:38 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
-
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Route::get('profile', function () {
+    return view('profile');
+});
+
+Route::get('/services/websites', function () {
+    return view('services.websites');
+})->name('services.websites');
+
+Route::get('/services/loja-virtual', function () {
+    return view('services.loja-virtual');
+})->name('services.loja-virtual');
+
+
+
+Route::post('/envia-contato', [ContatoController::class, 'enviar'])->name('envia.contato');
