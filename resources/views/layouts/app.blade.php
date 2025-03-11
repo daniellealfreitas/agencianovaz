ff68e37c5f36aac4db52a56a56d72791f0bd347d
Daniel Leal Freitas <dbleal@hotmail.com>
2025-03-11 11:21:17 -0300

    update services

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,14 @@
     <![endif]-->
 
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F6BN7CMC7M"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  gtag('config', 'G-F6BN7CMC7M');
+</script>
 
 <body>
     @if(session('success'))
